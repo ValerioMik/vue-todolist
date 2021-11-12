caa2683cb3b1be531da7a9d7487138ec2ee1b174
ValerioMik <valerio.avallone@hotmail.com>
2021-11-12 19:11:22 +0100

    manca una parte

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
 var app = new Vue(
     {
-        el:'#app',
-        data:{
-            newtodo:'',
+        el: '#app',
+        data: {
+            newtodo:
+                {
+                    testo: '',
+                    done: false
+                },
             todoList: [
                 {
-                    testo:'Fare la spesa ',
-                    done:true
+                    testo: 'Fare la spesa ',
+                    done: true
                 },
 
                 {
-                    testo:'Fare il bucato ',
+                    testo: 'Fare il bucato ',
+                    done: true
+                },
+                {
+                    testo: 'Fare un giro ',
+                    done: true
+                },
+
+                {
+                    testo: 'lavorare al computer ',
                     done: false
                 },
                 {
-                    testo:'Fare un giro ',
-                    done:true
+                    testo: 'fare un giro in città ',
+                    done: false
                 },
 
-                {
-                    testo:'lavorare al computer ',
-                    done:true
-                },
 
-                
             ]
+
+        },
+        
+        methods: {
+            aggiungitod() {
+                this.todoList.push(this.newtodo);
+                this.newtodo ={
+                    testo: '',
+                    done: false
+                }
+            },
+            eliminatodo(index){
+                this.todoList.splice(index,1)
+            },
+            toglirigaz(){
+                if(todoList.done === false){
+                    todoList.done = true
+                }else{
+                    
+                }
+            }
         
         }
-        
     }
 );
 
